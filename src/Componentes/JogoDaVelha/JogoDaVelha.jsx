9d89ecdf378b3180f428215903d641ae96660c26
Zion J <zfontana22@gmail.com>
2023-09-13 15:55:17 -0300

    Styling + icons

--- src/Componentes/JogoDaVelha/JogoDaVelha.jsx
+++ src/Componentes/JogoDaVelha/JogoDaVelha.jsx
@@ -1,9 +1,15 @@
 import React from 'react' 
 import './JogoDaVelha.css'
+import circle_icon from '../Assets/O-Letter.png'
+import x_icon from '../Assets/x.png'
+
 const JogoDaVelha = () => {
     return(
-        <div>
-
+        <div classname='container'>
+            <h1 classname='title'> Jogo Da Velha Em <span>React</span></h1>
+            <div classname= 'board'>
+             </div>
+        <button className="reset">Limpar</button>
         </div>
     )
 }
