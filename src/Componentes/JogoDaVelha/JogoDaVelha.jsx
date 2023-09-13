562b636a1e82e8445d501d5b3464837bd1b8e741
Zion J <zfontana22@gmail.com>
2023-09-13 16:07:25 -0300

    update?

--- src/Componentes/JogoDaVelha/JogoDaVelha.jsx
+++ src/Componentes/JogoDaVelha/JogoDaVelha.jsx
@@ -5,18 +5,18 @@ import x_icon from '../Assets/x.png'
 
 const JogoDaVelha = () => {
     return(
-        <div classname='container'>
-            <h1 classname='title'> Jogo Da Velha Em <span>React</span></h1>
-            <div classname= 'board'>
-                <div classname= 'row1'>
-                    <div classname= 'slots'></div>
-                    <div classname= 'slots'></div>
-                    <div classname= 'slots'></div>
+        <div classname="container">
+            <h1 classname="title"> Jogo Da Velha Em <span>React</span></h1>
+            <div classname= "board">
+                <div classname= "row1">
+                    <div classname= "slots"></div>
+                    <div classname= "slots"></div>
+                    <div classname= "slots"></div>
                 </div>
-                <div classname= 'row2'>
-                    <div classname= 'slots'></div>
-                    <div classname= 'slots'></div>
-                    <div classname= 'slots'></div>
+                <div classname= "row2">
+                    <div classname= "slots"></div>
+                    <div classname= "slots"></div>
+                    <div classname= "slots"></div>
                 </div>
                 <div classname= 'row3'>
                     <div classname= 'slots'></div>
