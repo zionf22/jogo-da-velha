432c57708f29b2e1e09afa578bf35191d93c4b51
Zion J <zfontana22@gmail.com>
2023-09-13 15:31:08 -0300

    Jogo Da Velha iniciar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import JogoDaVelha from './Componentes/JogoDaVelha/JogoDaVelha';
+
+function App() {
+  return (
+    <div>
+      <JogoDaVelha/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componentes/JogoDaVelha/JogoDaVelha.jsx
@@ -0,0 +1,11 @@
+import React from 'react' 
+import './JogoDaVelha.css'
+const JogoDaVelha = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default JogoDaVelha
